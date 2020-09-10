56c4f109a259d59cfd68bc8914348ec5cb5cfa2e
Daniel Widgren <daniel.widgren@gmail.com>
2020-09-10 20:47:20 +0200

    part1

--- src/controllers/github_demo_main_controller.erl
+++ src/controllers/github_demo_main_controller.erl
@@ -3,5 +3,20 @@
          index/1
         ]).
 
+-define(HEADERS, #{<<"Accept">> => <<"application/vnd.github.mercy-preview+json">>,
+                   <<"User-Agent">> => <<"Awesome-github-demo">>}).
+-define(GITHUB_API, <<"https://api.github.com/search/repositories?q=topic:">>).
+
 index(#{req := #{method := <<"GET">>}} = _NovaReq) ->
-    {ok, [{message, "Nova is running!"}]}.
+    Url = <<?GITHUB_API/binary, "erlang">>,   
+    logger:info("url: ~p", [Url]),
+    Options = #{close => true,
+                headers => ?HEADERS},
+    case shttpc:get(Url, Options) of
+        #{status := {200, _}, body := Body} ->
+            logger:info("~p", [Body]),
+            {ok, [{message, Body}]};
+        Result ->
+            logger:warning("did not go well: ~p", [Result]),
+            {ok, []}
+    end.
